927ea6a3687ab70ace33df053cbe661843645025
geetarbabara <41662458+geetarbabara@users.noreply.github.com>
2018-08-30 13:41:37 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = kwiNmzNUgpS0K0wJPktpWC4WSNOkVpwpJMWWmnGEyrdA6N4xOR1e+HUuhejW/M5vaRxM7orSc+Xwurf/P5MtNEImCsMPtds5p3FLITnUMy1I66o5QHgiAoG30YIKM/t5nyGCV4m7sXa2gLf9zQRWvwdB04t89/1O/w1cDnyilFU=;
 
 // Get POST body content
 $content = file_get_contents('php://input');
